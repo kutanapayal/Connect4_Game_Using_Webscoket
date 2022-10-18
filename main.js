62ff81dfca9f3e8782ae8568763ef6d6b7428b79
kutanapayal <payal.kutana@bacancy.com>
2022-10-18 12:30:28 +0530

    Change in Main.js Host...

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 import { createBoard, playMove } from "./connect4.js";
 
+function getWebSocketServer() {
+  if (window.location.host === "kutanapayal.github.io") {
+    return "wss://connect4-game-websocket.herokuapp.com/";
+  } else if (window.location.host === "localhost:8000") {
+    return "ws://localhost:8001/";
+  } else {
+    throw new Error(`Unsupported host: ${window.location.host}`);
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   // Initialize the UI.
   const board = document.querySelector(".board");
   createBoard(board);
   // Open the WebSocket connection and register event handlers.
-  const websocket = new WebSocket("ws://localhost:8001/");
+  const websocket = new WebSocket(getWebSocketServer());
   initGame(websocket);
   receiveMoves(board, websocket);
   sendMoves(board, websocket);
